16cb052691ac175ab0d1db30e03121bc346bdba9
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2021-11-13 23:06:29 +0530

    Create Floyd's_Triangle.c

---
+++ Floyd's_Triangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int i,j,k=1,n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    for(i = 1; i <= n; i++) {
+      for(j = 1;j <= i; j++)
+         printf("%4d", k++);
+      printf("\n");
+   }
+    getch();
+} 
